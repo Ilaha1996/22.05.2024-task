9e5d3afc09694644e1ca1d7c98d4851f1d1a1606
Ilaha1996 <ilahaah@code.edu.az>
2024-05-22 12:54:49 +0400

    Task added

---
+++ CarClass/CarClass/CarClass.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CarClass
+{
+    internal class Car
+    {
+        public string Brand;
+        public string Model;
+        public double CurrentFuel;
+        public double FuelFor1KM;
+        public double Millage;
+        public  void Drive (int km) 
+        {
+            
+            if (CurrentFuel>=FuelFor1KM*km)
+            {
+                Console.WriteLine(Millage += FuelFor1KM * km);
+
+            }
+            else 
+            {
+                Console.WriteLine("Benzin kifayet etmir." );
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ CarClass/CarClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CarClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Car car1 = new Car();
+            car1.Brand = "BMW";
+            car1.Model = "I7";
+            car1.CurrentFuel = 30;
+            car1.FuelFor1KM = 4;
+            car1.Millage = 10;
+
+            car1.Drive(7);
+
+            car1.FuelFor1KM = 10;
+            car1.Drive(7);
+
+        }
+    }
+}
